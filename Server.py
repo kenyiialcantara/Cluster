d95545dbfc91a40d656fac78381130fbf5ca7984
Kenyi Alcantara <116614545+kenyiialcantara@users.noreply.github.com>
2023-05-19 13:14:17 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ import socket
 import numpy as np
 import json
 import threading
-
+import re
 
 
 
@@ -12,9 +12,7 @@ class Server:
         self.port = 3000
         self.serv_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.clients = {}
-        self.coefficientes_fun_pol = [[7,1],[8,2]] #a0 x + a1 x + a2 x**2 + ... + an x**n
-        self.dominio = [5,10]
-        self.segmentos = 10000
+        self.dominio = [0,0]
         self.sumas_parciales = []
         self.threads_clients = []
         self.number_clients = 0
@@ -60,6 +58,7 @@ class Server:
         domains = np.linspace(self.dominio[0],self.dominio[1],len(self.clients)+1)
         data = {"function":self.coefficientes_fun_pol,'a':0,"b":0}
         data['segmentos']=self.segmentos
+        data['threads'] = self.threads
         i = 0
         for id in self.clients:
             data["a"] = domains[i]
@@ -75,12 +74,28 @@ class Server:
         
         #Configurando el numero de clientes
         self.number_clients = int(input('Ingresa el numero de clientes:'))
-        
+     
+        coeficientes = input("Ingrese los coeficientes separados por espacios: ").split()
+        exponentes = input("Ingrese los exponentes separados por espacios: ").split()
+    
+        coeficientes_exponentes = []
+    
+        for coeficiente, exponente in zip(coeficientes, exponentes):
+            coeficiente = float(coeficiente)
+            exponente = int(exponente)
+            coeficientes_exponentes.append([coeficiente, exponente])
+        self.coefficientes_fun_pol = coeficientes_exponentes
+
+        a = float(input("Ingresa a:")) 
+        b = float(input("Ingresa b:")) 
+        self.dominio = [a,b]
+        self.segmentos = int(input("Ingresa numero de segmentos:")) 
+        self.threads = int(input("Ingresa numero de threads que ejecutaran los clientes:")) 
         #Iniciando
         self.serv_socket.bind((self.host,self.port))
         #Experando conexion entrante
         self.serv_socket.listen()
-        print('El servidor esta escuchando en la ip {} y puerto {} ...'.format(self.host,self.port))
+        print('\nEl servidor esta escuchando en la ip {} y puerto {} ...'.format(self.host,self.port))
         self.resibe_clientes()
         for thread in self.threads_clients:
             thread.join()
@@ -102,3 +117,7 @@ def main():
 
 if __name__=="__main__":
     main()
+
+
+
+
